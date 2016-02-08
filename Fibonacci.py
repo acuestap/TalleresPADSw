015a2cba255b2f663cf03b8eb232c789a5370304
Abimelec Cuesta <acuestap@gmail.com>
2016-02-07 23:18:49 -0500

    Estos son los archivos base del primer ejercicio de python para la clase de Procesos Agiles de Desarrollo de Sofware

---
+++ Fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(contador,n,p1,p2):  
+ var = ""  
+ if(contador!=n):  
+  var=fibonacci(contador+1,n,p2,p1+p2)  
+  var=str(p2)+" "+var  
+ return var  
+n = int(input("Ingrese un numero entero\n"))  
+if(n>0):  
+ a=fibonacci(0,(n-1),0,1)  
+ print ("0 "+a)  
